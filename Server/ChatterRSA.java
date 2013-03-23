8a9397f01648e64025cf31f89eb73ddb87057787
Gage A. Black <black27@pod5-5.cs.purdue.edu>
2013-03-23 19:21:01 -0400

    added the ReadinKey and decript functions to ChatterRSA

--- Server/ChatterRSA.java
+++ Server/ChatterRSA.java
@@ -47,6 +47,31 @@ public class ChatterRSA {
 			oout.close();
 		}
 	}
+	PrivateKey readKeyFromFile(String keyFileName) throws IOException {
+		InputStream in =
+			ServerConnection.class.getResourceAsStream(keyFileName);
+		ObjectInputStream oin =
+			new ObjectInputStream(new BufferedInputStream(in));
+		try {
+			BigInteger m = (BigInteger) oin.readObject();
+			BigInteger e = (BigInteger) oin.readObject();
+			RSAPrivateKeySpec keySpec = new RSAPublicKeySpec(m, e);
+			KeyFactory fact = KeyFactory.getInstance("RSA");
+			PrivateKey priKey = fact.generatePrivate(keySpec);
+			return pubKey;
+		} catch (Exception e) {
+			throw new RuntimeException("Spurious serialisation error", e);
+		} finally {
+			oin.close();
+		}
+	}
+	public byte[] rsaDecrypt(byte[] data) {
+		PrivateKey priKey = readKeyFromFile("/.private.key");
+		Cipher cipher = Cipher.getInstance("RSA");
+		cipher.init(Cipher.DECRYPT_MODE, pubKey);
+		byte[] cipherData = cipher.doFinal(src);
+		return cipherData;
+	}
 
 
 	public static void main(String[] args) {
