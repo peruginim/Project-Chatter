6662369d65f2a5d399393a970aec223a6b5b2604
Ajay Panicker <panickeraj@gmail.com>
2013-03-31 02:43:50 -0400

    Added a special end character to both ChatterClient and Chatter server so that the client reads until it finds the special character. This fixes the problems with printing out statements with multiple newlines in it. Also in ChatterServer, please use printout rather than out.println to send to the client.

--- Server/ChatterServer.java
+++ Server/ChatterServer.java
@@ -11,26 +11,34 @@ public class ChatterServer extends Thread{
 	public void run(){
 			
 		try{
-	        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-	        BufferedReader in = new BufferedReader(
-	                new InputStreamReader(
-	                socket.getInputStream()));
-	        String inputLine, outputLine;
-	        ChatterProtocol cp = new ChatterProtocol();
-	        outputLine = cp.processInput(null);
-	        out.println(outputLine);
-	 
-	        while ((inputLine = in.readLine()) != null) {
-	             outputLine = cp.processInput(inputLine);
-	             out.println(outputLine);
-	             if (outputLine.equals("Bye."))
-	                break;
-	        }
-	        out.close();
-	        in.close();
-	        socket.close();
+			PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+			BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			String inputLine, outputLine;
+			ChatterProtocol cp = new ChatterProtocol();
+			outputLine = cp.processInput(null);
+			System.out.println(outputLine);
+			printout(out, outputLine);
+
+			while ((inputLine = in.readLine()) != null) {
+				outputLine = cp.processInput(inputLine);
+				System.out.println(inputLine+" : "+ outputLine+"\n");
+				printout(out, outputLine);
+				if (outputLine.equals("Bye."))
+					break;
+			}
+			out.close();
+			in.close();
+			socket.close();
 		}catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	//Use this instead of out.println, this way we can write multiple newlines to the client,
+	//instead of the message being \n terminated, it is terminated by a 0
+	private void printout(PrintWriter out, String string){
+		out.print(string);
+		out.write(0);
+		out.flush();
 	}
 }
