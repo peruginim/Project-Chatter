26e02763935ab524bf09bf7f262343e8ae5b0a76
ryan bennett <rbennett_91@yahoo.com>
2013-04-02 18:44:24 -0400

    attempt to connect to simple server

--- ProjectChatter/src/com/example/projectchatter/ConnectToServer.java
+++ ProjectChatter/src/com/example/projectchatter/ConnectToServer.java
@@ -6,37 +6,45 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 import android.app.Activity;
-import android.os.Bundle;
 
 
-public class ConnectToServer extends Activity {
+public class ConnectToServer{
 
 	int content = R.layout.settings;
 	Socket socket;
 	DataOutputStream DOS;
 	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // shows settings screen
-        setContentView(content);
-        
+    public ConnectToServer() {
         
         // create socket connection
 		try {
-			socket = new Socket("sslab20.purdue.edu", 4444);
-			DOS = new DataOutputStream(socket.getOutputStream());
-			DOS.writeUTF("HELLO_WORLD");
-			socket.close();
+			socket = new Socket("sslab14.cs.purdue.edu", 4444);
+			DOS = new DataOutputStream(socket.getOutputStream());;
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-
 		}
+		
+		if (socket != null && DOS != null){
+			try {
+				DOS.writeBytes("SUPPPP\n");
+				DOS.close();
+				socket.close();
+			}
+			catch (UnknownHostException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		
+		
     }
     
 }
