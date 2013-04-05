c3ed1f8e34cdf31c6ba21be371a3a38f236a92f0
Michael Perugini <mperugin@purdue.edu>
2013-04-04 20:01:26 -0400

    Changed the launcher icon to fit our microphone icon.
    Changed the name from Project Chatter to Chatter. This wont stay changed if people want to
    keep it Project Chatter.

--- ProjectChatter/gen/com/example/projectchatter/R.java
+++ ProjectChatter/gen/com/example/projectchatter/R.java
@@ -22,9 +22,10 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
-        public static final int mic=0x7f020001;
-        public static final int mic_icon=0x7f020002;
-        public static final int settings=0x7f020003;
+        public static final int ic_launcher_mic=0x7f020001;
+        public static final int mic=0x7f020002;
+        public static final int mic_icon=0x7f020003;
+        public static final int settings=0x7f020004;
     }
     public static final class id {
         public static final int action_settings=0x7f08000a;
@@ -49,13 +50,14 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
-        public static final int desc=0x7f05000b;
+        public static final int app_name_launcher=0x7f050008;
+        public static final int desc=0x7f05000c;
         public static final int hello_world=0x7f050002;
-        public static final int port=0x7f050008;
+        public static final int port=0x7f050009;
         public static final int record=0x7f050003;
         public static final int server_directory=0x7f050007;
-        public static final int settings=0x7f05000a;
-        public static final int settings_directory_default=0x7f050009;
+        public static final int settings=0x7f05000b;
+        public static final int settings_directory_default=0x7f05000a;
         public static final int settings_port_default=0x7f050006;
         public static final int settings_title=0x7f050005;
         public static final int welcome=0x7f050004;
